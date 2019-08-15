2232d0151170a21dd6d02073a975f4056cd3d9ea
John Reyes <john.jreye@gmail.com>
2019-08-15 03:31:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,10 @@ def introduction(name = 'Josh')
   puts "Hi, my name is #{name}."
 end
 
-
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end  
+  introduction_with_language("Dan", "Ember.js")
 
 
 
