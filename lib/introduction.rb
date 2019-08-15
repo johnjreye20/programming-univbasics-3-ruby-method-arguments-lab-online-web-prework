d016bdc0002b716dd21fb71231af3347f704b853
John Reyes <john.jreye@gmail.com>
2019-08-15 04:11:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,10 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
+
+
 =begin
+
 def introduction(name = 'Josh')
   puts "Hi, my name is #{name}."
 end
@@ -12,8 +15,11 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end  
   introduction_with_language("Dan", "Ember.js")
-end
+
+
 =end
+
+
 def introduction_with_language_optional(name, language ='Ruby')
   
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
