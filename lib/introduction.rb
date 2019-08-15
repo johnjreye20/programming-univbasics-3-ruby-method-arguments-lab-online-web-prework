97832e42671a8347fc3dec7622934d00538ec227
John Reyes <john.jreye@gmail.com>
2019-08-15 03:54:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,10 +14,10 @@ end
   introduction_with_language("Dan", "Ember.js")
 end
 
-def introduction_with_language_optional(name, language ="Ruby")
+def introduction_with_language_optional(name, language ="computer")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional("Edwin")
+introduction_with_language_optional("Edwin", "Ruby")
   
 
 
